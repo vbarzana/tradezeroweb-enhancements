d77b308af9ea1069cf3ed5c3fecb36bf56ca7f1a
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-07-16 10:23:55 +0000

    Finished the tradezero free web chrome extension, added some documentation about it

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tradezeroweb-enhancements
 A chrome extension to enhance the performance of tradezero web, as well as providing a better user experience.
+
+How to setup:
+* Download the extension from github
+* Extract the content into a folder
+* Open Chrome go to Settings -> More Tools -> Extensions
+* Enable "Developer Mode"
+* Click "Load unpacked" and select the folder you just downloaded from github
+* Go to the tradezero free trading tool and reload it [standard.tradezeroweb.com](https://standard.tradezeroweb.co/)
+
+Features so far:
+* Allows to collapse PNL section (shows up collapsed by default)
+* Clicking on the open positions symbol would add the Qty to the quantities so trading with multiple tickers we can quickly sell/buy.
+
+
+Note: You are free to use this tool, however I only use this for personal interest. The Tradezero web free tool is good enough, so with this feature I just add a few nits that I am willing to change so I don't have to wait for the next TZ release. This tool does not harm or affect TZ web at all, it is just a Chrome extension, is up to the developer to install any extension he wants in his chrome browser.
--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 'use strict';
 
-$(document).ready(function () {
-	$('head')
-		.append("<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,500,400italic,700,700italic' rel='stylesheet' type='text/css'>");
+window.Enhancer = {
+  startCollapsed: true,
+  isPnlCollapsed: false,
+  __init: function () {
+    this.addToggableSection();
+    if (this.startCollapsed) {
+      this.togglePnl();
+    }
+    setTimeout(this.bindPortfolioSymbolClicks.bind(this), 3000);
+  },
 
-	$('.trade-type').each(function () {
-		var item = $(this);
-		if (!item) {
-			return;
-		}
-		var text = item.text();
-		item.addClass(text === "Short Stock" ? 'fa fa-arrow-down' : text === "Long Stock" ? 'fa fa-arrow-up' : '');
-		item.css('display', 'block');
-		item.text('');
-	})
+  addToggableSection: function () {
+    this.ePnlSection = $('#page-header-container');
+    this.ePnlSubstitute = $('#trading-order-container-substitute');
+    this.ePnlSubstitute.remove();
+    this.toggleButton = $(
+      $(
+        '<span id="pnl-toggler" class="icon-box icon-toggle"></span>'
+      ).insertAfter(this.ePnlSection)[0]
+    );
+    this.toggleButton.on('click', this.togglePnl.bind(this));
+  },
+
+  togglePnl: function () {
+    if (!this.isPnlCollapsed) {
+      this.toggleButton.css({ transform: 'scaleY(1)' });
+      this.isPnlCollapsed = true;
+      this.ePnlSection.slideUp();
+    } else {
+      this.toggleButton.css({ transform: 'scaleY(-1)' });
+      this.isPnlCollapsed = false;
+      this.ePnlSection.slideDown();
+    }
+  },
+
+  bindPortfolioSymbolClicks: function () {
+    var allSymbols = $('#portfolio-contents-1 .table-2.opTable tr').find(
+      'td:first'
+    );
+
+    allSymbols.each(
+      function (pos, symbol) {
+        var symbol = $(symbol);
+        symbol.on('click', this.handleSymbolClick.bind(this, symbol));
+      }.bind(this)
+    );
+  },
+
+  handleSymbolClick: function (symbol) {
+    var eQuantity = $('#trading-order-input-quantity');
+    eQuantity.val(symbol.next().next().html());
+  }
+};
+
+$(document).ready(function () {
+  window.Enhancer.__init();
 });
